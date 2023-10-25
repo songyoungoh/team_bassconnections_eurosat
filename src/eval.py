0bbced4341c1d4bdd5d27c494cfb4f67ec4562f3
DaryLu0v0 <147575954+DaryLu0v0@users.noreply.github.com>
2023-10-25 02:24:21 -0400

    Update eval.py

--- src/eval.py
+++ src/eval.py
@@ -1,3 +1,4 @@
+# Import necessary libraries and modules for deep learning, data visualization, and data processing
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -5,7 +6,6 @@ import matplotlib.pyplot as plt
 from torchvision import models
 import os
 import numpy as np
-import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -18,11 +18,17 @@ from torch.utils.data import random_split
 from collections import Counter
 import pandas as pd
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
+
+# Function to evaluate a model and get the confusion matrix
 def eval_confusion(model, dataloaders):
+    # Set the device to CUDA if available, otherwise CPU.
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    
+    # Set the model to evaluation mode
     model.eval()
+
+    # Lists to store the true labels and model's predictions
     true_labels = []
     predictions = []
 
@@ -33,36 +39,49 @@ def eval_confusion(model, dataloaders):
     correct_probs = []
     total_probs = []
 
+    # Loop through the validation data
     for idx, (inputs, labels) in enumerate(dataloaders['valid']):
-      inputs = inputs.to(device)
-      labels = labels.to(device)
+        # Move the inputs and labels to the appropriate device (GPU or CPU)
+        inputs = inputs.to(device)
+        labels = labels.to(device)
 
-      with torch.no_grad():
-          outputs = model(inputs)
-          _, preds = torch.max(outputs, 1)
+        # Ensure no gradients are calculated as we are only evaluating, not training
+        with torch.no_grad():
+            # Forward pass to get the model's predictions
+            outputs = model(inputs)
+            # Get the index of the predicted class
+            _, preds = torch.max(outputs, 1)
 
-      true_labels.extend(labels.cpu().numpy())
-      predictions.extend(preds.cpu().numpy())
+        # Extend the true labels and predictions lists
+        true_labels.extend(labels.cpu().numpy())
+        predictions.extend(preds.cpu().numpy())
 
-      # Store the incorrectly and correctly classified images and their probabilities
-      for i, (pred, label) in enumerate(zip(preds, labels)):
-          total_probs.append(outputs[i].cpu().numpy())
-          if pred != label:
-              incorrect_indices.append(idx * dataloaders['valid'].batch_size + i)
-              incorrect_probs.append(outputs[i].cpu().numpy())
-          else:
-              correct_indices.append(idx * dataloaders['valid'].batch_size + i)
-              correct_probs.append(outputs[i].cpu().numpy())
+        # Store the incorrectly and correctly classified images and their probabilities
+        for i, (pred, label) in enumerate(zip(preds, labels)):
+            total_probs.append(outputs[i].cpu().numpy())
+            if pred != label:
+                incorrect_indices.append(idx * dataloaders['valid'].batch_size + i)
+                incorrect_probs.append(outputs[i].cpu().numpy())
+            else:
+                correct_indices.append(idx * dataloaders['valid'].batch_size + i)
+                correct_probs.append(outputs[i].cpu().numpy())
 
-    # Get a test accuracy
+    # Compute the test accuracy
     test_acc = np.sum(np.array(true_labels) == np.array(predictions)) / len(true_labels)
     print('')
     print(f'Test Accuracy: {test_acc * 100:.2f}%')
     print(f'Number of Correct Predictions: {np.sum(np.array(true_labels) == np.array(predictions))} / 5400')
     print(f'Number of Incorrect Predictions: {len(true_labels) - np.sum(np.array(true_labels) == np.array(predictions))} / 5400')
 
-    # Save strings of class labels
+    # Get the string representations of class labels
     class_labels = dataloaders['valid'].dataset.dataset.classes
+
+    # Map integer class indices to their corresponding string class labels
+    true_labels_str = [class_labels[i] for i in true_labels]
+    predictions_str = [class_labels[i] for i in predictions]
+    
+    return true_labels_str, predictions_str
+
 
     # Map integer values with string values
     true_labels_str = [class_labels[i] for i in true_labels]
